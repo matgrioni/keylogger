786d870295a193367f624616fbb6280378092a7c
Matias Grioni <matgrioni@gmail.com>
2016-11-23 00:12:12 -0500

    Simple sniffer of single TCP packet using pcap.

--- main.c
+++ main.c
@@ -1,60 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include <sys/socket.h>
-#include <netinet/tcp.h>
-#include <netinet/ip.h>
-#include <netinet/if_ether.h>
-#include <unistd.h>
+#include <stdlib.h>
+
+#include <pcap.h>
 
 #define TCP_PROTOCOL 6
 
+/* TODO: Can these be used in pcap filter */
 #define HTTP_PORT 80
 #define HTTPS_PORT 443
 
-static const int PACKET_BUFFER_SIZE = 65536;
+/* TODO: Uncomment this. static const int PCAP_ERRBUF_SIZE = 100; */
 
 int main()
 {
-    int socket_fd;
-    int packet_size;
-    struct sockaddr conn;
-    char packet[PACKET_BUFFER_SIZE];
+    char *dev, errbuf[100];
+    pcap_t *handle;
+    struct bpf_program filter;
+    bpf_u_int32 mask;
+    bpf_u_int32 net;
+    struct pcap_pkthdr header;
+    const u_char *packet;
 
-    socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (socket_fd < 0)
+    /* Create the default pcap device and exit if error occurs */
+    dev = pcap_lookupdev(errbuf);
+    if (dev == NULL)
     {
-        printf("Error creating socket. Exiting...");
+        printf("No default device: %s. Exiting...", errbuf);
         exit(1);
     }
 
-    while (true)
+    /* Find the netmask for the chosen device */
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) < 0)
     {
-        socklen_t conn_size = sizeof(conn);
-        packet_size = recvfrom(socket_fd, packet, PACKET_BUFFER_SIZE, 0, &conn, &conn_size);
-        
-        if (packet_size < 0)
-        {
-            printf("Error receiving packet. Exiting...");
-            exit(1);
-        }
-
-        struct iphdr *iph = (struct iphdr*) packet;
-        if (iph->protocol == TCP_PROTOCOL)
-        {
-            unsigned short ip_header_len = iph->ihl * 4;
-            struct tcphdr *tcph = (struct tcphdr*) (packet + ip_header_len);
-
-            int total_header_size = ip_header_len + tcph->doff * 4;
-
-            for (int i = total_header_size; i < packet_size; i++)
-            {
-                printf("%c", (unsigned int) packet[i]);
-            }
-        }
+        printf("Can't get netmask for device: %s. Exiting...", errbuf);
+        exit(1);
     }
 
-    close(socket_fd);
+    /* Create a handle to start sniffint packets in non promiscuous
+       mode. */
+    handle = pcap_open_live(dev, BUFSIZ, false, 3000, errbuf);
+    if (handle == NULL)
+    {
+        printf("Couldn't open device: %s. Exiting...", errbuf);
+        exit(1);
+    }
+
+    /* Handle the creation of the filter compilation and installing */
+    if (pcap_compile(handle, &filter, "tcp port 80 or tcp port 443", 0, net) < 0)
+    {
+        printf("Couldn't create filter: %s. Exiting...", pcap_geterr(handle));
+        exit(1);
+    }
+
+    if (pcap_setfilter(handle, &filter) < 0)
+    {
+        printf("Couldn't install filter: %s. Exiting...", pcap_geterr(handle));
+        exit(1);
+    }
+
+    packet = pcap_next(handle, &header);
+    printf("%d", header.len);
+    pcap_close(handle);
 
     return 0;
 }
